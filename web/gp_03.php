0e068cdfbde20568b68a8588084a6394d6881576
Michael Goytia <goy11002@gmail.com>
2019-01-23 21:06:43 -0600

    group assignment 02

--- web/gp_03.php
+++ web/gp_03.php
@@ -7,15 +7,25 @@
 
 </head>
 <body>
-<p>apple</p>
 <?php
 $username = $_POST['name'];
 $email = $_POST['email'];
 $major = $_POST['major'];
 $comments = $_POST['comment'];
-$continent = $_POST['continents'];
+$continents = $_POST['continents'];
 
-echo "$username";
+$contMap = array('na'=>'North American','sa'=>'South American', 'eu'=>'Europe', 'asia'=>'Asia', 'afr'=>'Africa',
+    'ant'=>'Antarctica', 'aus'=>'Australia');
+
+
+echo "$username<br>";
+echo "$email<br>";
+echo "$major<br>";
+echo "$comments<br>";
+foreach ($continents as $con) {
+    echo "$contMap[$con]<br>";
+}
+
 ?>
 
 </body>
--- web/group_assignment03.php
+++ web/group_assignment03.php
@@ -5,34 +5,44 @@
 </head>
 
 <body>
+
 <form method="post" action="gp_03.php">
     <input type="text" name="name" placeholder="Name">
     <br>
     <input type="text" name="email" placeholder="Email">
     <br>
-    <input type="radio" name="major" value="Computer Science">Computer Science
-    <br>
-    <input type="radio" name="major" value="Web Design and Development">Web Design and Development
-    <br>
-    <input type="radio" name="major" value="Computer Information Technology">Computer Information Technology
-    <br>
-    <input type="radio" name="major" value="Computer Engineering">Computer Engineering
+
+    <?php
+        $majors = array('Computer Science', 'Web Design and Development','Computer Information Technology'
+        ,'Computer Engineering');
+
+        foreach ($majors as $major){
+            echo "<input type='radio' name='major' value='$major'>$major<br>";
+        }
+    ?>
+    <!--    <input type="radio" name="major" value="Computer Science">Computer Science-->
+<!--    <br>-->
+<!--    <input type="radio" name="major" value="Web Design and Development">Web Design and Development-->
+<!--    <br>-->
+<!--    <input type="radio" name="major" value="Computer Information Technology">Computer Information Technology-->
+<!--    <br>-->
+<!--    <input type="radio" name="major" value="Computer Engineering">Computer Engineering-->
     <br>
     <textarea name="comment"></textarea>
-
-    <input type="checkbox" name="continents" value="na">North America
     <br>
-    <input type="checkbox" name="continents" value="sa">South America
+    <input type="checkbox" name="continents[]" value="na">North America
     <br>
-    <input type="checkbox" name="continents" value="eu">Europe
+    <input type="checkbox" name="continents[]" value="sa">South America
     <br>
-    <input type="checkbox" name="continents" value="asia">Asia
+    <input type="checkbox" name="continents[]" value="eu">Europe
     <br>
-    <input type="checkbox" name="continents" value="aus">Australia
+    <input type="checkbox" name="continents[]" value="asia">Asia
     <br>
-    <input type="checkbox" name="continents" value="afr">Africa
+    <input type="checkbox" name="continents[]" value="aus">Australia
     <br>
-    <input type="checkbox" name="continents" value="ant">Antarctica
+    <input type="checkbox" name="continents[]" value="afr">Africa
+    <br>
+    <input type="checkbox" name="continents[]" value="ant">Antarctica
     <br>
     <input type="submit" value="Submit">
 </form>
