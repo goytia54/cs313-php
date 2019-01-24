cea0c6788ecea05aef556af2a484eb63d869e346
Michael Goytia <goy11002@gmail.com>
2019-01-23 20:41:33 -0600

    group assignment 02

---
+++ web/gp_03.php
@@ -0,0 +1,22 @@
+
+
+
+<html>
+<head>
+
+
+</head>
+<body>
+<p>apple</p>
+<?php
+$username = $_POST['name'];
+$email = $_POST['email'];
+$major = $_POST['major'];
+$comments = $_POST['comment'];
+$continent = $_POST['continents'];
+
+echo "$username";
+?>
+
+</body>
+</html>
