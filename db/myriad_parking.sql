cd3ae73d3f05eb339ccce89ac11804bb6af11c19
Michael Goytia <goy11002@gmail.com>
2019-02-02 20:05:16 -0600

    week 4 assignment update

--- db/myriad_parking.sql
+++ db/myriad_parking.sql
@@ -32,3 +32,17 @@ create table myriad_parking.GROUP_MAPPING(
   GROUP_ID int references myriad_parking.GROUPS(GROUP_ID),
   USER_ID int references myriad_parking.PARKING_USERS(user_id)
 );
+
+
+--==========ABOVE AND BEYOND===============--
+insert into myriad_parking.PARKING_USERS (USER_ID, USER_NAME, FIRST_NAME, LAST_NAME, EMAIL, PASSWORD, SPOT_ID)
+values (1,'mgoytia','Michael','Goytia','mgoytia@myriad.com','123degree',null);
+
+insert into myriad_parking.BUILDINGS (BUILDING_ID, BUILDING_NAME)
+values (1,'Building 1');
+
+insert into myriad_parking.GROUPS (GROUP_ID, GROUP_NAME)
+values (1,'SP-LAB');
+
+insert into myriad_parking.GROUP_MAPPING (group_map_id, group_id, user_id)
+values (1,1,1);
