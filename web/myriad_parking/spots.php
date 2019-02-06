d2494b005f4902320d828c59b6d644b4fa414496
Michael Goytia <goy11002@gmail.com>
2019-02-06 15:40:51 -0600

    week 5 assignment

--- web/myriad_parking/spots.php
+++ web/myriad_parking/spots.php
@@ -1,5 +1,37 @@
 <?php
-    include_once ('navbar.php');
-    include 'dbconect.php';
-    $db = getDBConnection();
+include_once ('navbar.php');
+include 'dbconect.php';
+$db = getDBConnection();
 
+?>
+
+
+<html>
+
+<head></head>
+<body>
+<div id="group-div">
+    <?php echo '<ul class="list-group">';
+    foreach ($db->query('SELECT * FROM myriad_parking.PARKING_SPOTS') as $row)
+    {
+        $active = $row['active_flag'];
+        $available = $row['available'];
+        if($active == 1 && $available == 1){
+            $building = $row['build_id'];
+            $spot_number = $row['spot_number'];
+            $level = $row['level'];
+            echo '<li class="list-group-item">'.$building. '-'.$level.'-'.$spot_number.'-'.'</li>';
+        }
+    }
+    echo '</ul>';
+    ?>
+</div>
+
+</body>
+<style>
+    #group-div{
+        width: 200px;
+        height: 300px;
+    }
+</style>
+</html>
