41da200945e628d115ce88f50a25685c3d18e772
Michael Goytia <goy11002@gmail.com>
2019-02-05 23:04:46 -0600

    week 5 assignment

---
+++ web/dbtest.php
@@ -0,0 +1,36 @@
+<?php
+    try
+    {
+        $dbUrl = getenv('DATABASE_URL');
+
+        echo $dbUrl;
+
+        $dbOpts = parse_url($dbUrl);
+
+        $dbHost = $dbOpts["host"];
+        $dbPort = $dbOpts["port"];
+        $dbUser = $dbOpts["user"];
+        $dbPassword = $dbOpts["pass"];
+        $dbName = ltrim($dbOpts["path"],'/');
+
+        $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+
+        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    }
+    catch (PDOException $ex)
+    {
+        echo 'Error!: ' . $ex->getMessage();
+        $user = 'postgres';
+        $password = 'yud72heo';
+        $db = new PDO('pgsql:host=localhost;dbname=mydb', $user, $password);
+        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    }
+
+    foreach ($db->query('SELECT * FROM myriad_parking.PARKING_USERS') as $row)
+    {
+        echo 'user: ' . $row['USER_ID'];
+        echo ' password: ' . $row['USER_NAME'];
+        echo '<br/>';
+    }
+
+?>
