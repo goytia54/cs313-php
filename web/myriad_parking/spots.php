7bdf98fa51e2242021e81cf6eba307b55f7ccebe
Michael Goytia <goy11002@gmail.com>
2019-02-06 15:47:29 -0600

    week 5 assignment

--- web/myriad_parking/spots.php
+++ web/myriad_parking/spots.php
@@ -12,15 +12,16 @@ $db = getDBConnection();
 <body>
 <div id="group-div">
     <?php echo '<ul class="list-group">';
-    foreach ($db->query('SELECT * FROM myriad_parking.PARKING_SPOTS') as $row)
+    foreach ($db->query('select * from myriad_parking.parking_spots inner join myriad_parking.buildings
+on parking_spots.build_id = buildings.building_id') as $row)
     {
         $active = $row['active_flag'];
         $available = $row['available'];
         if($active == 1 && $available == 1){
-            $building = $row['build_id'];
+            $building = $row['building_name'];
             $spot_number = $row['spot_number'];
             $level = $row['level'];
-            echo '<li class="list-group-item">'.$building. '-'.$level.'-'.$spot_number.'-'.'</li>';
+            echo '<li class="list-group-item">'.$building.'-'.$level.'-'.$spot_number.'</li>';
         }
     }
     echo '</ul>';
