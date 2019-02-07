8aa953e2a4f7f9fd92b76d65348fa7ad6550faae
Michael Goytia <goy11002@gmail.com>
2019-02-06 20:41:54 -0600

    group assignment 5

--- web/group_work/scripture.php
+++ web/group_work/scripture.php
@@ -1,4 +1,9 @@
-<?php
+<html>
+    <head>
+
+    </head>
+    <body>
+    <?php
     include '../dbconect.php';
 
     try {
@@ -8,12 +13,25 @@
         echo $exception->getMessage();
     }
 
-    foreach ($db->query('select * from public.SCRIPTURES') as $row){
+    $searchBook = $_GET['book'];
+
+    foreach ($db->query("select * from public.SCRIPTURES where book = '$searchBook'") as $row){
         $book = $row['book'];
         $chapter = $row['chapter'];
         $verse = $row['verse'];
         $content = $row['content'];
 
-        echo '<b>'. $book. $chapter.':'. $verse. '</b>-' . $content.'<br>';
+        echo '<b>'. $book. ' '.$chapter.':'. $verse. '</b>-' . $content.'<br>';
     };
-?>
+    ?>
+
+    <form method="GET" action="scripture.php">
+        <input type="text" name="book">
+
+        <button type="submit">Submit</button>
+    </form>
+
+    </body>
+
+</html>
+
