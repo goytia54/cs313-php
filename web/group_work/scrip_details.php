9d4b003490221740858d1a453fec2553d38d07b2
Michael Goytia <goy11002@gmail.com>
2019-02-06 21:03:02 -0600

    group assignment 5

--- web/group_work/scrip_details.php
+++ web/group_work/scrip_details.php
@@ -6,6 +6,12 @@
     $db = getDBConnection();
     foreach ($db->query("select content from public.scriptures where id=$id")as $row){
         $content = $row['content'];
-        echo $content;
+        $book = $row['book'];
+        $chapter = $row['chapter'];
+        $verse = $row['verse'];
+        echo "<p>$content</p>";
+        echo "<p>$book $chapter:$verse</p>";
+
+
     };
 ?>
