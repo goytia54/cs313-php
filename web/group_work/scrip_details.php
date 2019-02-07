8fe231d7bc3146208be8d0307967e4c93c9b9ba8
Michael Goytia <goy11002@gmail.com>
2019-02-06 20:59:50 -0600

    week 5 assignment

--- web/group_work/scrip_details.php
+++ web/group_work/scrip_details.php
@@ -5,7 +5,7 @@
 
     $db = getDBConnection();
     foreach ($db->query("select content from public.scriptures where id=$id")as $row){
-        $content = $row[$content];
+        $content = $row['content'];
         echo $content;
     };
 ?>
